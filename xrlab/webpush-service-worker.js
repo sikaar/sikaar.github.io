a4763cdb458aa796bbbae392c140808d6682c641
Sikaar <sikaar@gmail.com>
2020-07-02 18:16:31 +0200

    Update webpush-service-worker.js

--- xrlab/webpush-service-worker.js
+++ xrlab/webpush-service-worker.js
@@ -1,1 +1,3 @@
-
+importScripts('https://api.pushio.com/webpush/sdk/service_min.js');
+var config = '{"appserviceKey":"BNhNAoMke3hTRRvGCjr3CnW5QyHcbhF77p_GluYUFH7xVjFLKSiFRZTwni0wiitMltkTa1Q58PiFRJYPQ3wKrOM=","apiKey":"ABEnUUY7kwh3K2F4zv5ETZPgI","accountToken":"ABEkJLaLTQTtCZNQ-oRVel1Z0","appver":"0.0.0","apiHost":"https://api.pushio.com","lazy":false}';
+orawp.init(config);
