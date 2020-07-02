53646ef662c78a06e4fbc57616172efc50b8ecf0
Sikaar <sikaar@gmail.com>
2020-07-02 17:42:22 +0200

    Add files via upload

---
+++ webpush-service-worker.js
@@ -0,0 +1,3 @@
+importScripts('https://api.pushio.com/webpush/sdk/service_min.js');
+var config = '{"appserviceKey":"BIkR9gI8aQVu47MblpbIQbYPSuJOuxuAG63Rw6p_aKjHY_x6iXjap6CFLLJz3lhIeY4BYytECM0wa2JDPrXLP4w=","apiKey":"ABEr7q7piBeI9CJvEOxIivbV8","accountToken":"ABEkJLaLTQTtCZNQ-oRVel1Z0","appver":"0.0.0","apiHost":"https://api.pushio.com","lazy":false}';
+orawp.init(config);
