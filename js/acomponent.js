26c865c17b4308744353f5173a93164ca6b32b98
Sikaar <sikaar@gmail.com>
2023-12-05 09:58:05 +0100

    Update acomponent.js

--- js/acomponent.js
+++ js/acomponent.js
@@ -6,7 +6,7 @@ for (var i=0; i<50; i++)
 if (i<25){this.el.innerHTML += "<a-entity gltf-model='#blue' position='" + addCube()+ "' ></a-entity>"};
 //console.log("adding one");
 //console.log("<a-sphere radius=.1 color=" + getRandomColor() +" position=" + addCube() + "></a-sphere>");
-this.el.innerHTML += "<a-plane scale='.2 .2 .2' color=" + getRandomColor() +" position='" + addCube() + "' look-at='#player'> <a-entity text='width: 4; color: #ffffff; value: BLEU; align: center' position ='0 0 .1' background='color: #ffffff'></a-entity></a-plane>";
+this.el.innerHTML += "<a-plane scale='.2 .2 .2' src=" + getRandomTexture() +" color=" + getRandomColor() +" position='" + addCube() + "' look-at='#player'> <a-entity text='width: 4; color: #ffffff; value: BLEU; align: center' position ='0 0 .1' background='color: #ffffff'></a-entity></a-plane>";
 }
   }
 });
