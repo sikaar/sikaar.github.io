60936262bf324633366b81511698a86fd8a3bd39
Sikaar <sikaar@gmail.com>
2024-04-29 10:42:01 +0200

    Update acomponent.js

--- js/acomponent.js
+++ js/acomponent.js
@@ -3,7 +3,7 @@ AFRAME.registerComponent('containerent', {
     let container = this.el;
 for (var i=0; i<50; i++)
 {
-if (i<30){this.el.innerHTML += "<a-entity gltf-model='#blue' position='" + addCube()+ "' ></a-entity>"};
+if (i<30){this.el.innerHTML += "<a-entity gltf-model='#blue' position='" + addCube()+ "' look-at='#player' ></a-entity>"};
 //console.log("adding one");
 //console.log("<a-sphere radius=.1 color=" + getRandomColor() +" position=" + addCube() + "></a-sphere>");
 this.el.innerHTML += "<a-plane scale='.2 .2 .2' src=" + getRandomTexture() +" color=" + getRandomColor() +" position='" + addCube() + "' look-at='#player'> <a-entity text='width: 4; color: #ffffff; value: BLEU; align: center' position ='0 0 .1' background='color: #ffffff'></a-entity></a-plane>";
